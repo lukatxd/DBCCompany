630c2425fbed8b945131294f1906b1a81ab975cb
Lucas Bittencourt Nogueira <33840832+lukatxd@users.noreply.github.com>
2021-06-26 16:15:47 -0300

    Initial commit

---
+++ VotingCentral/src/main/java/com/dbc/votingcentral/VotingCentralApplication.java
@@ -0,0 +1,13 @@
+package com.dbc.votingcentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingCentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingCentralApplication.class, args);
+	}
+
+}
---
+++ VotingCentral/src/test/java/com/dbc/votingcentral/VotingCentralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbc.votingcentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingCentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
